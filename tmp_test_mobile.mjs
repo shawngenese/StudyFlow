c44fa3171c198d30103cae083b6270e5e0217b9d
shawngenese <shawngenese25@gmail.com>
2026-08-27 20:24:10 +0800

    removed obolete images files and test scripts

--- tmp_test_mobile.mjs
+++
@@ -1,5 +0,0 @@
-import { chromium } from 'playwright';
-const browser = await chromium.launch();
-const preview = await new Promise((res,rej)=>{
-  const {spawn} = await import('node:child_process');
-});
